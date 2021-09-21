74b62874c48b92224bbda91185a42d9a7ed4df0d
Build <jwbradley2003@gmail.com>
2021-09-21 08:32:23 -0400

    Testing time

--- ScpSpeech/Patches/SpeechPatch.cs
+++ ScpSpeech/Patches/SpeechPatch.cs
@@ -8,30 +8,61 @@
 namespace ScpSpeech.Patches
 {
 #pragma warning disable SA1313
-    using Assets._Scripts.Dissonance;
+    using System;
     using Exiled.API.Features;
     using Exiled.Permissions.Extensions;
     using HarmonyLib;
+    using Mirror;
+    using PlayableScps;
+    using PlayableScps.Messages;
 
     /// <summary>
-    /// Patches <see cref="DissonanceUserSetup.UserCode_CmdAltIsActive"/> to implement scps speaking to humans.
+    /// Patches <see cref="Scp939.ServerReceivedVoiceMsg"/> to implement scps speaking to humans.
     /// </summary>
-    [HarmonyPatch(typeof(DissonanceUserSetup), nameof(DissonanceUserSetup.UserCode_CmdAltIsActive))]
+    [HarmonyPatch(typeof(Scp939), nameof(Scp939.ServerReceivedVoiceMsg))]
     internal static class SpeechPatch
     {
-        private static bool Prefix(DissonanceUserSetup __instance, bool value)
+        private static bool Prefix(NetworkConnection conn, Scp939VoiceMessage msg)
         {
-            Player player = Player.Get(__instance.gameObject);
-            if (player == null || !player.IsScp)
-                return true;
+            if (!ReferenceHub.TryGetHubNetID(conn.identity.netId, out ReferenceHub hub))
+                return false;
+
+            Player player = Player.Get(hub);
+            if (!player.IsScp)
+                return false;
 
             if (Plugin.Instance.Config.GlobalTalking.Contains(player.Role) ||
-                player.CheckPermission($"ss.{player.Role.ToString().ToLower()}"))
+                player.CheckPermission($"ss.{ScpToString(player.Role).ToLower()}"))
             {
-                __instance.MimicAs939 = value;
+                hub.dissonanceUserSetup.MimicAs939 = msg.IsMimicking;
             }
 
             return false;
+        }
+
+        private static string ScpToString(RoleType roleType)
+        {
+            switch (roleType)
+            {
+                case RoleType.Scp173:
+                    return nameof(RoleType.Scp173);
+                case RoleType.Scp106:
+                    return nameof(RoleType.Scp106);
+                case RoleType.Scp049:
+                    return nameof(RoleType.Scp049);
+                case RoleType.Scp079:
+                    return nameof(RoleType.Scp079);
+                case RoleType.Scp096:
+                    return nameof(RoleType.Scp096);
+                case RoleType.Scp0492:
+                    return nameof(RoleType.Scp0492);
+                case RoleType.Scp93953:
+                    return nameof(RoleType.Scp93953);
+                case RoleType.Scp93989:
+                    return nameof(RoleType.Scp93989);
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(roleType));
+            }
         }
     }
 }
